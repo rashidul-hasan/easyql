342431879f57b17dcfe569849eb9716744f6a131
Rashidul Hasan <rashidul69@gmail.com>
2024-10-14 12:23:04 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Rashidul\EasyQL\Http\Controllers\CrudController;
 
-Route::group([ 'prefix' => config('easyql.api_prefix')], function () {
+Route::group([ 'prefix' => config('easyql.api_prefix'), 'middleware' => config('easyql.middleware')], function () {
     Route::get('/crud/schema', [CrudController::class, 'schema']);
     Route::post('/crud/find-where', [CrudController::class, 'findWhere']);
     Route::resource('/crud', CrudController::class);
