e4a450c088e068e8ff4eab8cfeb047016cc3b000
rashidul <rashidul69@gmail.com>
2024-12-04 17:28:54 +0600

    Fix crash during initial install due to missing config

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Rashidul\EasyQL\Http\Controllers\CrudController;
 
-Route::group([ 'prefix' => config('easyql.api_prefix'), 'middleware' => array_merge(config('easyql.middleware'), ['easyql.check.query'])], function () {
+Route::group([ 'prefix' => config('easyql.api_prefix'), 'middleware' => array_merge(config('easyql.middleware') ?? [], ['easyql.check.query'])], function () {
     Route::get('/crud/schema', [CrudController::class, 'schema']);
     Route::post('/crud/find-where', [CrudController::class, 'findWhere']);
     Route::resource('/crud', CrudController::class);
